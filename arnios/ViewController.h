71caba4e8dc75aff0d966cfb25498b5133031b2c
cinna <wangxingnan@Cinna.local>
2014-12-05 23:49:22 +0800

    Initial Commit

---
+++ arnios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  arnios
+//
+//  Created by cinna on 14-12-5.
+//  Copyright (c) 2014年 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ arnios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  arnios
+//
+//  Created by cinna on 14-12-5.
+//  Copyright (c) 2014年 Personal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
