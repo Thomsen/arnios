dbf21a649d3c6e987a6b866278f2eb0459390430
Thomsen <changsen4584@163.com>
2018-04-26 12:04:59 +0800

    add AppDependencies

---
+++ arnios/AppDependencies.h
@@ -0,0 +1,22 @@
+//
+//  AppDependencies.h
+//  arnios
+//
+//  Created by Thomsen Wang on 31/1/2018.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#ifndef AppDependencies_h
+#define AppDependencies_h
+
+
+@interface AppDependencies : NSObject
+
+- (void) installRootViewControllerIntoWindow: (UIWindow *) window;
+
+@end
+
+#endif /* AppDependencies_h */
---
+++ arnios/AppDependencies.m
@@ -0,0 +1,38 @@
+//
+//  AppDependencies.m
+//  arnios
+//
+//  Created by Thomsen Wang on 31/1/2018.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+#import "AppDependencies.h"
+
+//@interface AppDependencies ()
+
+//@property (nonatomic, strong) TodoListWireframe *listWireframe;
+
+//@end
+
+@implementation AppDependencies
+
+- (void) installRootViewControllerIntoWindow:(UIWindow *) window {
+    
+}
+
+
+- (id) init {
+    if ((self = [super init])) {
+        [self configureDependencies];
+    }
+    
+    return self;
+}
+
+
+- (void) configureDependencies {
+    
+}
+
+@end
+
